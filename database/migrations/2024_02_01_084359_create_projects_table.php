315330377fd65cfe488096b1af6a06fddd519d56
MrElle <cassaralavoro@gmail.com>
2024-02-01 10:41:43 +0100

    add crud

--- database/migrations/2024_02_01_084359_create_projects_table.php
+++ database/migrations/2024_02_01_084359_create_projects_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 50);
+            $table->text('description');
+            $table->string('image');
+            $table->string('topic', 50);
             $table->timestamps();
         });
     }
--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,16 @@ class ProjectSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $array_project = config('projects');
+
+        foreach($array_project as $project_item)
+        {
+            $array_project = new Project();
+            $array_project->title = $project_item["title"];
+            $array_project->description = $project_item["description"];
+            $array_project->image = $project_item["image"];
+            $array_project->topic = $project_item["topic"];
+            $array_project->save();
+        }
     }
 }
